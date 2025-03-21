b12daa2316f25423a9aafb71eeb933650a7eccad
takoyaki-3 <hatano.yuuta@gmail.com>
2025-03-22 08:19:34 +0900

    Initial commit

---
+++ lib/takoyaki3-com-gallery-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Takoyaki3ComGalleryStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Takoyaki3ComGalleryQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/takoyaki3-com-gallery.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Takoyaki3ComGallery from '../lib/takoyaki3-com-gallery-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/takoyaki3-com-gallery-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Takoyaki3ComGallery.Takoyaki3ComGalleryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
